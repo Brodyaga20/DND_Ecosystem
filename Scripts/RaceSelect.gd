b69f3df9e94af3e8ad879f858ebb85ccaf9602b5
Brodyaga20 <artiom.poz@mail.ru>
2026-09-13 01:50:30 +0300

    style(ui): change style of choosing race from buttons to highlight(test version)

--- Scripts/RaceSelect.gd
+++ Scripts/RaceSelect.gd
@@ -1,8 +1,9 @@
-extends Control
+extends Node2D
 
 var subclass = ""
 var wayback = ""
 var selected_race_id = ""
+var preselected_race_id = ""
 
 func _ready():
 	subclass = TempData.subclass_id
@@ -12,37 +13,11 @@ func _ready():
 		wayback = "res://Scenes/RogueSubclassSelect.tscn"
 	else:
 		wayback = "res://Scenes/WarriorSubclassSelect.tscn"
-	TempData.reset()  # сброс временных данных
+	for child in get_tree().get_nodes_in_group("characters"):
+		child.visible = false
 	$Next.disabled = true
 	$Blur/NameLabel.text = ""
 	$Blur/DescriptionLabel.text = ""
-
-
-	
-
-func _on_gnome_button_pressed() -> void:
-	select_race("gnome")
-
-func _on_chitin_button_pressed():
-	select_race("chitin")
-
-func _on_human_button_pressed() -> void:
-	select_race("human")
-
-func _on_aarakokra_button_pressed() -> void:
-	select_race("aarakocra")
-
-func _on_giant_button_pressed() -> void:
-	select_race("giant")
-
-func _on_aasimar_button_pressed() -> void:
-	select_race("aasimar")
-
-func _on_owlin_button_pressed() -> void:
-	select_race("owlin")
-
-func _on_drow_button_pressed() -> void:
-	select_race("drow")
 
 func select_race(race_id: String):
 	selected_race_id = race_id
@@ -57,7 +32,75 @@ func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file(wayback)
 	pass # Replace with function body.
 
-
 func _on_next_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/SexSelect.tscn")
 	pass # Replace with function body.
+
+func clear_highlight():
+	for child in get_tree().get_nodes_in_group("characters"):
+		child.visible = false
+	pass
+
+func _on_gnome_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			clear_highlight()
+			$Gnome/Gnome.visible = true
+			select_race("gnome")
+	pass
+
+func _on_chitin_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			clear_highlight()
+			$Chitin/Chitin.visible = true
+			select_race("chitin")
+	pass
+
+func _on_human_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			clear_highlight()
+			$Human/Human.visible = true
+			select_race("chitin")
+	pass
+
+func _on_aarakocra_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			clear_highlight()
+			$Aarakocra/Aarakocra.visible = true
+			select_race("aarakocra")
+	pass
+
+func _on_giant_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			clear_highlight()
+			$Giant/Giant.visible = true
+			select_race("giant")
+	pass
+
+func _on_aasimar_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			clear_highlight()
+			$Aasimar/Aasimar.visible = true
+			select_race("aasimar")
+	pass
+
+func _on_owlin_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			clear_highlight()
+			$Owlin/Owlin.visible = true
+			select_race("owlin")
+	pass
+
+func _on_drow_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			clear_highlight()
+			$Drow/Drow.visible = true
+			select_race("drow")
+	pass
